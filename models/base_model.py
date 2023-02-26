e19901fb6bf5b000deaf7863007b4d7a347c1d5c
nadaMAoui <maaouinada747@gmail.com>
2023-02-26 20:51:11 +0100

    updates tsk 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,34 @@
 import uuid
 from datetime import datetime
+from models import storage
+
 
 class BaseModel:
-    """
-    Defines common attributes/methods for other classes.
-    """
     def __init__(self, *args, **kwargs):
-        """
-        Initializes the instance with a unique id and the creation date.
-        """
         if kwargs:
             for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
+                    setattr(self, key, value)
+            self.id = kwargs.get('id') or str(uuid.uuid4())
+            self.created_at = kwargs.get('created_at') or datetime.now()
+            self.updated_at = kwargs.get('updated_at') or datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """
-        Returns a string representation of the instance.
-        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        Updates the public instance attribute updated_at with the current datetime.
-        """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
-        """
-        dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = self.__class__.__name__
-        dict_copy['created_at'] = self.created_at.isoformat()
-        dict_copy['updated_at'] = self.updated_at.isoformat()
-        return dict_copy
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
