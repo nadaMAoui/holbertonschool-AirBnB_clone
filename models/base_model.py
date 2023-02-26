56b8f291dac91b0c26d8329a30d638c42c262311
nadaMAoui <maaouinada747@gmail.com>
2023-02-26 19:48:01 +0100

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -39,3 +39,15 @@ class BaseModel:
         self.updated_at = datetime.now()
         models.storage.save()
 
+    def to_dict(self):
+        """
+        Return __dict__ value, key.
+        Convert Str to iso format.
+        serialization and deserialization.
+        """
+
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
