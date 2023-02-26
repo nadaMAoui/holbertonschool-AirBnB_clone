06a76e6d6de8b158165f8134f478bf09fad718f3
nadaMAoui <maaouinada747@gmail.com>
2023-02-26 21:25:24 +0100

    main class

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""class base model"""
+'''base model class'''
 
-import uuid
-from datetime import datetime
 from models import storage
 
 
@@ -14,24 +12,18 @@ class BaseModel:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
-            self.id = kwargs.get('id') or str(uuid.uuid4())
-            self.created_at = kwargs.get('created_at') or datetime.now()
-            self.updated_at = kwargs.get('updated_at') or datetime.now()
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
-
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = self.updated_at = datetime.now()
+                storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
