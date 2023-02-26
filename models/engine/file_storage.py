bc125ed03818d02ff03b735457b492ddabf6d4a7
Salma5806 <5806@holbertonstudents.com>
2023-02-26 22:31:25 +0100

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,41 +1,56 @@
 #!/usr/bin/python3
 """
-Filestorage funtion all, new, save, reload
+Filestorage funtion all, new, save, reload, delete, get
 """
-import json
-import os.path
-from models.base_model import BaseModel
 from models.user import User
+from models.base_model import BaseModel
+import json
 
 
-class FileStorage():
-    """ Init"""
+class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """ Returns dict """
-        return type(self).__objects
+    def all(self, cls=None):
+        if cls is None:
+            return self.__objects
+        else:
+            result = {}
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    result[key] = value
+            return result
 
     def new(self, obj):
-        """ Sets new object in dictionary """
-        if obj:
-            self.__objects["{}.{}".format(
-                obj.__class__.__name__, obj.id)] = obj
+        key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects[key] = obj
 
     def save(self):
-        """ Serializes __objects = {}"""
-        new_jhoson_file = {}
-        with open(self.__file_path, mode="w") as jfile:
+        with open(self.__file_path, mode='w', encoding='utf-8') as file:
+            new_dict = {}
             for key, value in self.__objects.items():
-                new_jhoson_file[key] = value.to_dict()
-                """JSON encoder and decoder"""
-            json.dump(new_jhoson_file, jfile)
+                new_dict[key] = value.to_dict()
+            file.write(json.dumps(new_dict))
 
     def reload(self):
-        """ Deserializes __objects = {}"""
-        if os.path.isfile(self.__file_path):
-            with open(self.__file_path, mode="r") as jfile:
-                for key, value in json.load(jfile).items():
-                    new_obj = eval(value["__class__"])(**value)
-                    self.__objects[key] = new_obj
+        try:
+            with open(self.__file_path, mode='r', encoding='utf-8') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name = key.split('.')[0]
+                    if class_name == "User":
+                        obj = User(**value)
+                    else:
+                        obj = BaseModel(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            del self.__objects[key]
+
+    def get(self, cls, id):
+        key = cls.__name__ + '.' + id
+        return self.__objects.get(key, None)
