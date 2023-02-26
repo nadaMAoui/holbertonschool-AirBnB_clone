c6a795cab599cb2b7caf9d41ad0cb204c0f37bda
nadaMAoui <maaouinada747@gmail.com>
2023-02-26 20:46:55 +0100

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -9,9 +9,17 @@ class BaseModel:
         """
         Initializes the instance with a unique id and the creation date.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
