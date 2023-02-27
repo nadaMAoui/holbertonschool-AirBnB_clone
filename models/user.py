2faa378f2dfddd3099539b89db8c0e49e5923244
nadaMAoui <maaouinada747@gmail.com>
2023-02-27 09:08:40 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""user class"""
-
+""" Class user. """
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
+    """ Attributes user. """
     email = ""
     password = ""
     first_name = ""
